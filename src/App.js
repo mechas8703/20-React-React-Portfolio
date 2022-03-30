a657c659a50d3ca3e5364cdf1956bda378850ef4
Maria Cardona <mechas8703@hotmail.com>
2022-03-30 19:13:43 -0400

    fixing errors

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // Importing components
-import NavBar from './components/Nav';
-import Header from './components/Header';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import NavBar from './components/nav';
+import Header from './components/header';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import App from './App';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
-), )
+// import BrowserRouter from 'react-router-dom/BrowserRouter'
+
+// ReactDOM.render((
+//    <BrowserRouter basename={process.env.PUBLIC_URL}>
+//      <App />
+//    </BrowserRouter>
+// ), )
