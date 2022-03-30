e5ce117987f4c37512301759812ba47edb9115ee
Maria Cardona <mechas8703@hotmail.com>
2022-03-30 11:59:52 -0400

    App.js

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// Importing components
+import NavBar from './components/Nav';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+       < NavBar />
+       < Header />
+       < Portfolio /> 
+       < Contact />
+       < Footer /> 
+    </div>
+  );
+}
+
+export default App;
